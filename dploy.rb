2e70f210653b1bc6399b45e8f1737a2dc14a4de1
baturayofluoglu <baturay@dataroots.io>
2020-04-08 16:40:16 +0200

    echo ouput

--- dploy.rb
+++ dploy.rb
@@ -12,7 +12,7 @@ class Dploy < Formula
 
   def install
     ENV['DPLOY_HOST'] = "https://api.dev.dinosl1ke1cecream.dploy.ai/"
-    echo ENV['DPLOY_HOST']
+    system echo ENV['DPLOY_HOST']
     bin.install "dploy"
   end
 end
