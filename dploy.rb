41eca022b06667a53bb9e0935922b3ffbe9c956e
baturayofluoglu <baturay@dataroots.io>
2020-04-08 19:56:44 +0200

    remove version

--- dploy.rb
+++ dploy.rb
@@ -2,8 +2,8 @@ class Dploy < Formula
   desc "Deployment Tool"
   homepage "https://github.com/dploy-ai"
   url "http://temp-cli.storage.googleapis.com/dploy"
-  sha256 "d6436abc52ffa038b013fa22f4a5e8fcfb6cb1f863278c2b743470ee8d141d8a"
-  version "0.0.0"
+  sha256 "no_check"
+  version "latest"
 
   # depends_on "curl"
 
@@ -14,8 +14,8 @@ class Dploy < Formula
     bin.install "dploy"
   end
 
-  def post_install
-    system "echo alias dploy=\'DPLOY_HOST=\"https://api.dev.dinosl1ke1cecream.dploy.ai/\" dploy\' >> ~/.bash_profile"
-    system "echo alias dploy=\'DPLOY_HOST=\"https://api.dev.dinosl1ke1cecream.dploy.ai/\" dploy\' >> ~/.zshrc"
-  end
+  # def post_install
+  #   system "echo alias dploy=\'DPLOY_HOST=\"https://api.dev.dinosl1ke1cecream.dploy.ai/\" dploy\' >> ~/.bash_profile"
+  #   system "echo alias dploy=\'DPLOY_HOST=\"https://api.dev.dinosl1ke1cecream.dploy.ai/\" dploy\' >> ~/.zshrc"
+  # end
 end
