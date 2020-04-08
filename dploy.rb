b64fa14a01d5733ed84f090c98fc38bf4f4b891b
baturayofluoglu <baturay@dataroots.io>
2020-04-08 16:35:29 +0200

    break commit sha

--- dploy.rb
+++ dploy.rb
@@ -2,11 +2,11 @@ class Dploy < Formula
   desc "Deployment Tool"
   homepage "https://github.com/dploy-ai"
   url "http://temp-cli.storage.googleapis.com/dploy"
-  sha256 "33a454346608470e25bd040cbeaf5089554a294898fe408ce4fc9fe164473c28"
+  sha256 "3a454346608470e25bd040cbeaf5089554a294898fe408ce4fc9fe164473c28"
   version "0.0.0"
 
   # depends_on "curl"
-  
+
   # Don't need to compile / Already compiled
   bottle :unneeded
 
