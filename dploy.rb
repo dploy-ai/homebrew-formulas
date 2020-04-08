152098230597b62931592a7c0ed05fa3c6cc6a57
Bart6114 <bart@dataroots.io>
2020-04-08 20:56:52 +0200

    seems to work

--- dploy.rb
+++ dploy.rb
@@ -1,19 +1,23 @@
 class Dploy < Formula
-  desc "Deployment Tool"
-  homepage "https://github.com/dploy-ai"
+  desc "dploy.ai CLI"
+  homepage "https://dploy.ai/"
   url "http://temp-cli.storage.googleapis.com/dploy"
-  sha256 "" # Don't need integration test because we don't use versions for now
-  version "latest"
+  sha256 "" # don't check for SHAs now so we can reinstall latest dev version
+  version "0.0.1"
 
   # Don't need to compile / Already compiled
   bottle :unneeded
 
   def install
+    system "mv", "dploy", "dploy-bare"
+    system "bash", "-c", "printf '#!/bin/bash\nDPLOY_HOST=https://api.dev.dinosl1ke1cecream.dploy.ai/ dploy-bare \"$@\"' >> dploy" 
+    bin.install "dploy-bare"
     bin.install "dploy"
   end
 
-  # def post_install
-  #   system "echo alias dploy=\'DPLOY_HOST=\"https://api.dev.dinosl1ke1cecream.dploy.ai/\" dploy\' >> ~/.bash_profile"
-  #   system "echo alias dploy=\'DPLOY_HOST=\"https://api.dev.dinosl1ke1cecream.dploy.ai/\" dploy\' >> ~/.zshrc"
-  # end
+  def caveats
+    <<~EOS
+      Let's go! 🎉 Just run `dploy`.
+    EOS
+  end
 end
