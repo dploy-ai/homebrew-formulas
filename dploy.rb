2947e73962b8ab486feecdf2b147e1f6c7e96ee8
baturayofluoglu <baturay@dataroots.io>
2020-04-08 16:06:38 +0200

    add dploy host

--- dploy.rb
+++ dploy.rb
@@ -5,7 +5,7 @@ class Dploy < Formula
   sha256 "3236838b2b56f567b083882a45abb996a4fdc811f45ab5ffda3f398097049e52"
   version "0.0.0"
 
-  # depends_on "curl"
+  $DPLOY_HOST="https://api.dev.dinosl1ke1cecream.dploy.ai/"
 
   # Don't need to compile / Already compiled
   bottle :unneeded
