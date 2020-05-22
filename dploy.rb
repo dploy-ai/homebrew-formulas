2a663afdfd2e69a07a7e803c87d1cbb4612f7bc8
Romain FOYARD <romain@dploy.ai>
2020-05-22 11:13:07 +0200

    Updated formula to match recent dploy cli changes

--- dploy.rb
+++ dploy.rb
@@ -3,15 +3,12 @@ class Dploy < Formula
   homepage "https://dploy.ai/"
   url "http://temp-cli.storage.googleapis.com/dploy"
   sha256 "" # don't check for SHAs now so we can reinstall latest dev version
-  version "0.0.1"
+  version "0.0.2"
 
   # Don't need to compile / Already compiled
   bottle :unneeded
 
   def install
-    system "mv", "dploy", "dploy-bare"
-    system "bash", "-c", "printf '#!/bin/bash\nDPLOY_HOST=https://api.dev.dinosl1ke1cecream.dploy.ai dploy-bare \"$@\"' >> dploy" 
-    bin.install "dploy-bare"
     bin.install "dploy"
   end
 
