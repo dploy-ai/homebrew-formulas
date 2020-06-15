3aaf4b7d0767340d4d527d667abb2a09c720f3e2
Bart6114 <bart@dploy.ai>
2020-06-15 09:23:11 +0200

    update formula to match targz filename

--- dploy.rb
+++ dploy.rb
@@ -1,17 +1,17 @@
 class Dploy < Formula
   def self.latest_dploy_revision
     @latest_dploy_revision ||= begin
-      Date.parse(`curl --silent --HEAD 'http://temp-cli.storage.googleapis.com/dploy-darwin-amd64' | grep 'Last-Modified:'`.split(' ', 2).last.strip).to_s
+      Date.parse(`curl --silent --HEAD 'http://temp-cli.storage.googleapis.com/dploy-darwin-amd64.tar.gz' | grep 'Last-Modified:'`.split(' ', 2).last.strip).to_s
     end
   end
 
   def self.sha256_checksum
-    `curl --silent 'http://temp-cli.storage.googleapis.com/dploy-darwin-amd64.sha256'`.split.first
+    `curl --silent 'http://temp-cli.storage.googleapis.com/dploy-darwin-amd64.tar.gz.sha256'`.split.first
   end
 
   desc "dploy.ai CLI"
   homepage "https://dploy.ai/"
-  url "http://temp-cli.storage.googleapis.com/dploy-darwin-amd64"
+  url "http://temp-cli.storage.googleapis.com/dploy-darwin-amd64.tar.gz"
   sha256 sha256_checksum
   version "0.1-#{latest_dploy_revision}"
 
@@ -19,8 +19,7 @@ class Dploy < Formula
   bottle :unneeded
 
   def install
-    bin.install "dploy-darwin-amd64"
-    mv bin/"dploy-darwin-amd64", bin/"dploy"
+    bin.install "dploy"
   end
 
   def caveats
