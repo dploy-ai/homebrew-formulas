1f70810e9dfa520fc8a273570a2a21f3ec384ebb
Bart6114 <bart@dataroots.io>
2020-08-06 11:17:06 +0200

    add dploy-dev and dploy formulas

---
+++ dploy-dev.rb
@@ -0,0 +1,28 @@
+class DployDev < Formula
+  def self.latest_dploy_revision
+      `curl --silent 'https://storage.googleapis.com/dployai-cli/dploy-darwin-amd64-dev-info.txt' | grep version: | grep -o "[^version: ].*$"`.strip
+  end
+
+  def self.sha256_checksum
+    `curl --silent 'https://storage.googleapis.com/dployai-cli/dploy-darwin-amd64-dev.tar.gz.sha256'`.split.first
+  end
+
+  desc "dploy.ai CLI"
+  homepage "https://dploy.ai/"
+  url "http://dployai-cli.storage.googleapis.com/dploy-darwin-amd64-dev.tar.gz"
+  sha256 sha256_checksum
+  version "#{latest_dploy_revision}"
+
+  # Don't need to compile / Already compiled
+  bottle :unneeded
+
+  def install
+    bin.install "dploy"
+  end
+
+  def caveats
+    <<~EOS
+      Let's go! 🎉 Just run `dploy`.
+    EOS
+  end
+end
--- dploy.rb
+++ dploy.rb
@@ -1,30 +1,28 @@
 class Dploy < Formula
-  def self.latest_dploy_revision
-    @latest_dploy_revision ||= begin
-      DateTime.parse(`curl --silent --HEAD 'http://storage.googleapis.com/dployai-cli/dploy-darwin-amd64-dev.tar.gz' | grep 'Last-Modified:'`.split(' ', 2).last.strip).to_s
+    def self.latest_dploy_revision
+        `curl --silent 'https://storage.googleapis.com/dployai-cli/dploy-darwin-amd64-latest-info.txt' | grep version: | grep -o "[^version: ].*$"`.strip
+    end
+  
+    def self.sha256_checksum
+      `curl --silent 'https://storage.googleapis.com/dployai-cli/dploy-darwin-amd64-latest.tar.gz.sha256'`.split.first
+    end
+  
+    desc "dploy.ai CLI"
+    homepage "https://dploy.ai/"
+    url "http://dployai-cli.storage.googleapis.com/dploy-darwin-amd64-latest.tar.gz"
+    sha256 sha256_checksum
+    version "#{latest_dploy_revision}"
+  
+    # Don't need to compile / Already compiled
+    bottle :unneeded
+  
+    def install
+      bin.install "dploy"
+    end
+  
+    def caveats
+      <<~EOS
+        Let's go! 🎉 Just run `dploy`.
+      EOS
     end
   end
-
-  def self.sha256_checksum
-    `curl --silent 'http://storage.googleapis.com/dployai-cli/dploy-darwin-amd64-dev.tar.gz.sha256'`.split.first
-  end
-
-  desc "dploy.ai CLI"
-  homepage "https://dploy.ai/"
-  url "http://dployai-cli.storage.googleapis.com/dploy-darwin-amd64-dev.tar.gz"
-  sha256 sha256_checksum
-  version "0.1-#{latest_dploy_revision}"
-
-  # Don't need to compile / Already compiled
-  bottle :unneeded
-
-  def install
-    bin.install "dploy"
-  end
-
-  def caveats
-    <<~EOS
-      Let's go! 🎉 Just run `dploy`.
-    EOS
-  end
-end
